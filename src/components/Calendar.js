bf35f77d0efb75f567da22c0c842d94d97cc5c53
shambashib20 <shambashibmajumdar2000@gmail.com>
2021-09-02 02:10:58 +0530

    changes.

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -4,7 +4,7 @@ import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
 
 //calender that will be shown on our main page
-export default function CalenderApp () {
+export default function CalendarApp () {
 //main date functions to initialize our date state
 const [dateState, setDateState] = useState(new Date())
 //changes date to current selection on calender
@@ -15,16 +15,16 @@ return (
     //returns the calender as rendered cal
     <div className="container-fluid">
       <div className="favorites-header">
-            <h2>CALENDER</h2>
+            <h2>CALENDAR</h2>
          </div>
-      <div className="calender-main">
+      <div className="calendar-main">
         <div className="card">
-          <div className="calender-card-body">
+          <div className="calendar-card-body">
             <>
             <Calendar 
             value={dateState}
             onChange={changeDate}
-            className="calender-body"
+            className="calendar-body"
             />
           </>
         </div>
